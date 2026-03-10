cc6f711a67149cec80f43d45e823d578a2853110
sunshinericky0615 <sunshinericky.0615@icloud.com>
2026-03-10 10:49:27 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import HERO from "./components/HERO";
+import About from "./components/About";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HERO />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import SocialLinks from "./SocialLinks";
+import PageLinks from "./PageLinks";
+import logo from "../assets/TBC.png";
+
+const Navbar = () => {
+  const [isToggled, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!isToggled);
+  };
+  return (
+    <nav className="navbar">
+      <div className="container navbar-flex">
+        <a href="#home" className="logo">
+          <img src={logo} alt="logo" className="logo" />
+        </a>
+        <div className="main-menu">
+          <ul className="main-menu-list">
+            <PageLinks />
+          </ul>
+          <ul className="main-menu-icons">
+            <SocialLinks />
+          </ul>
+        </div>
+        <div className="mobile-menu">
+          <div className="mobile-menu-toggle" onClick={handleToggle}>
+            <i className="fa-solid fa-bars"></i>
+          </div>
+          <div className="mobile-menu-items">
+            <ul className={`mobile-menu-list ${isToggled ? "active" : ""}`}>
+              <PageLinks />
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
